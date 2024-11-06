a9a7bc96862b7bec50bced01f22915e04d039f9c
rexjmn <rexmayorga97@gmail.com>
2024-11-06 23:43:44 +0100

    forth 2 commit

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,28 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  
-  assetsInclude: [
-    '**/*.glb',
-    '**/*.gltf',
-    '**/*.hdr',
-    '**/*.png',
-    '**/*.jpg',
-    '**/*.jpeg'
-  ],
-
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    minify: 'esbuild', // Cambiado de 'terser' a 'esbuild'
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor': ['react', 'react-dom'],
-          'three': ['three'],
-          'gsap': ['gsap'],
+    plugins: [react()],
+    assetsInclude: [
+        '**/*.glb',
+        '**/*.gltf',
+        '**/*.hdr',
+        '**/*.png',
+        '**/*.jpg',
+        '**/*.jpeg'
+    ],
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: true,
+        minify: 'esbuild',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'vendor': ['react', 'react-dom'],
+                    'three': ['three'],
+                    'gsap': ['gsap'],
+                }
+            }
         }
-      }
     }
-  }
 });
