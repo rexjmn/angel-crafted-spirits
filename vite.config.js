8487cad661dea2c609dbdd86a47303d73b831c4b
rexjmn <rexmayorga97@gmail.com>
2024-11-06 04:44:17 +0100

    seconde commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'], // Include GLB files as assets
+  
+  assetsInclude: [
+    '**/*.glb',
+    '**/*.gltf',
+    '**/*.hdr',
+    '**/*.png',
+    '**/*.jpg',
+    '**/*.jpeg'
+  ],
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    minify: 'esbuild', // Cambiado de 'terser' a 'esbuild'
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom'],
+          'three': ['three'],
+          'gsap': ['gsap'],
+        }
+      }
+    }
+  }
 });
