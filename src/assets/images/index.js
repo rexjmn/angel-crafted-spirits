63bef7e12f8146eb8aeef0e08db60768ab04eb87
rexjmn <rexmayorga97@gmail.com>
2024-11-06 23:02:37 +0100

    forth 1 commit

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -10,7 +10,4 @@ import replayImg from './replay.svg';
 import rightImg from './right.svg';
 import watchImg from './watch.svg';
 import Newsletter from './newsletter-bg.png';
-import Image1 from './image1.png';
-import Image2 from './image2.png';
-import Image3 from './image3.png';
-export { Angel, AngelBlack, heroVideo, smallHeroVideo,  SatyrScene, playImg, pauseImg, replayImg, rightImg, watchImg, Newsletter, Image1, Image2, Image3 };
+export { Angel, AngelBlack, heroVideo, smallHeroVideo,  SatyrScene, playImg, pauseImg, replayImg, rightImg, watchImg, Newsletter};
